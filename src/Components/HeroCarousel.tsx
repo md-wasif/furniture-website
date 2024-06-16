e6b15965a21afedfa255681530e96638b08ed971
dev@patelgtech.com <md.wasif7894@gmail.com>
2024-06-17 00:37:53 +0530

    banner padding fixes

--- src/Components/HeroCarousel.tsx
+++ src/Components/HeroCarousel.tsx
@@ -1,7 +1,8 @@
 "use client";
 import React, { useEffect, useState } from 'react';
 import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
-import { RxDotFilled } from 'react-icons/rx';
+import { RiArrowLeftSLine } from 'react-icons/ri';
+import { SlArrowRight, SlArrowLeft } from 'react-icons/sl';
 import Image from 'next/image';
 
 function HeroCarousel() {
@@ -53,19 +54,19 @@ function HeroCarousel() {
   }, [currentIndex]); 
 
   return (
-    <div className='flex h-15rem'>
-      <div className='max-w-[1200px] h-[580px] w-[60%] m-auto relative group pr-1'>
+    <div className='flex h-15rem pt-2 pl-2'>
+      <div className='max-w-[1200px] h-[580px] w-[60%] m-auto relative group pr-2.5'>
         <div
           style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
           className='w-full h-full bg-center bg-cover duration-500'
         ></div>
         {/* Left Arrow */}
-        <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-2 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
-          <BsChevronCompactLeft onClick={prevSlide} size={30} />
+        <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] text-2xl rounded-full p-2 bg-white text-black cursor-pointer'>
+          <SlArrowLeft onClick={prevSlide} size={25} />
         </div>
         {/* Right Arrow */}
-        <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-2 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
-          <BsChevronCompactRight onClick={nextSlide} size={30} />
+        <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-2 text-2xl rounded-full p-2 bg-white text-black cursor-pointer'>
+          <SlArrowRight onClick={nextSlide} size={25} />
         </div>
         <div className='flex top-4 justify-center py-2'>
           {slides.map((slide, slideIndex) => (
@@ -78,11 +79,11 @@ function HeroCarousel() {
           ))}
         </div>
       </div>
-      <div className='w-[40%] h-full flex flex-col justify-center items-center'>
-        <div className='w-full h-[50%]'>
+      <div className='w-[40%] h-full flex flex-col justify-center items-center pt-2'>
+        <div className='w-full h-[50%] pr-2'>
           <Image className='w-full h-[290px] object-cover' src="https://res.cloudinary.com/dg38njbya/image/upload/v1716636832/website%20assets/fabric%20sofa/cqftsk51nynrekacmaji.webp" alt="" width={1920} height={200} />
         </div>
-        <div className='w-full h-[50%] pt-1'>
+        <div className='w-full h-[50%] pt-2.5 pr-2'>
           <Image className='w-full h-[290px] object-cover' src="https://res.cloudinary.com/dg38njbya/image/upload/v1716636832/website%20assets/fabric%20sofa/rq6jlb6miocjw0p2jnzj.webp" alt="" width={1920} height={200} />
         </div>
       </div>
